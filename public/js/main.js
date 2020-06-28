7cd2d3d1cc3a0c10985caf857a5469d4d2fcf790
Cristiano Solarino <cristiano.solarino@gmail.com>
2020-06-28 22:55:42 +0100

    added frontend class

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,60 @@
+class ProGBarZ {
+	constructor() {
+		this.barz = {}
+		this.add('.progress', 0.5)
+		this.init()
+	}
 
-window.onload = function onLoad() {
-    var bar = new ProgressBar.Line('.progress', {
-	 	strokeWidth: 4,
-	  	easing: 'easeInOut',
-	  	duration: 1400,
-	  	color: '#FFEA82',
-	  	trailColor: '#eee',
-	  	trailWidth: 1,
-	  	svgStyle: {width: '100%', height: '5%'},
-	  	text: {
-	    	style: {
-		      	// Text color.
-		      	// Default: same as stroke color (options.color)
-		      	color: '#999',
-		      	right: '0',
-		      	top: '30px',
-		      	padding: 0,
-		      	margin: 0,
-		      	transform: null
-	    	},
-	    	autoStyleContainer: false
-	  	},
-	  	from: {color: '#FFEA82'},
-	  	to: {color: '#ED6A5A'},
-	  	step: (state, bar) => {
-	    	bar.setText(Math.round(bar.value() * 100) + ' %');
-	  	}
-	});
+	init() {
+		const self = this
+		window.onload = () => {
+			document.getElementById('incr-1').onclick = (event) => {
+				self.increment('.progress', -1)
+			}
+		}
+	}
 
-	bar.animate(0.5); 
-};
+	add(selector, value) {
+		var bar = new ProgressBar.Line(selector, {
+			strokeWidth: 4,
+		  	easing: 'easeInOut',
+		  	duration: 1400,
+		  	color: '#FFEA82',
+		  	trailColor: '#eee',
+		  	trailWidth: 1,
+		  	svgStyle: {width: '100%', height: '5%'},
+		  	text: {
+		    	style: {
+			      	// Text color.
+			      	// Default: same as stroke color (options.color)
+			      	color: '#999',
+			      	right: '0',
+			      	top: '30px',
+			      	padding: 0,
+			      	margin: 0,
+			      	transform: null
+		    	},
+		    	autoStyleContainer: false
+		  	},
+		  	from: {color: '#FFEA82'},
+		  	to: {color: '#ED6A5A'},
+		  	step: (state, bar) => {
+		    	bar.setText(Math.round(bar.value() * 100) + ' %');
+		  	}
+		})
+		bar.animate(value);
+		this.barz[selector] = {bar: bar, value: value} 
+	}
+
+	increment(selector, percent) {
+		const bar = this.barz[selector].bar
+
+		if (bar) {
+			const value = this.barz[selector].value + percent*0.01
+			bar.set(value)
+			this.barz[selector].value = value
+		}
+	}
+}
+
+const PgbZ = new ProGBarZ()
