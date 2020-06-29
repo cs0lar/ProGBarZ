9658104242d610c376e4ded7512f30fc67176ff2
Cristiano Solarino <cristiano.solarino@gmail.com>
2020-06-29 13:49:03 +0100

    moved styles to css file + added dates to task table schema

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS pgbz_task (
 	id          INTEGER PRIMARY KEY,
 	name		TEXT NOT NULL,
 	description TEXT NOT NULL,
+	created_at  TEXT NOT NULL,
+	updated_at  TEXT NOT NULL,
 	progress    INTEGER NOT NULL DEFAULT 0
 );
